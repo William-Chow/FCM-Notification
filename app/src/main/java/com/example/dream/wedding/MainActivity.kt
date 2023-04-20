c1cf2671cdca658d19173500f9fab5e5fda0e9ec
williamchow <weelim1120@gmail.com>
2023-04-20 13:42:21 +0800

    add permission for Android 33

--- app/src/main/java/com/example/dream/wedding/MainActivity.kt
+++ app/src/main/java/com/example/dream/wedding/MainActivity.kt
@@ -1,15 +1,32 @@
 package com.example.dream.wedding
 
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.android.gms.tasks.*
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.FirebaseApp
 import com.google.firebase.messaging.FirebaseMessaging
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvNotification : TextView
+    lateinit var btnButton : Button
+
+    lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+    private val notificationManager: NotificationManager by lazy { getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,5 +41,26 @@ class MainActivity : AppCompatActivity() {
             Log.i("William", "This is the token : " + task.result
             )
         }
+        tvNotification = findViewById(R.id.tvNotification)
+        btnButton = findViewById(R.id.btnNotification)
+        btnButton.setOnClickListener{
+            if(ContextCompat.checkSelfPermission(this@MainActivity, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+                }
+            }
+        }
+
+        requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            verifyUI()
+            if(!it){
+                Snackbar.make(findViewById<View>(android.R.id.content).rootView, "Please grant Notification permission from App Settings", Snackbar.LENGTH_LONG).show()
+            }
+        }
+        verifyUI()
+    }
+
+    private fun verifyUI(){
+        tvNotification.text = if(notificationManager.areNotificationsEnabled()) "TRUE" else "FALSE"
     }
 }
